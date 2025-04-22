b8223267b0c15ef38701f17289eaad390de7f565
animeshsahu <anisahu99@gmail.com>
2025-04-22 16:02:26 +0530

    1

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x:i32 = -123;
+    let y:u8 = 3;
+    let z:f32 = 12.223;
+
+    println!("x:{x}");
+    println!("y:{y}");
+    print!("z:{z}");
+}
